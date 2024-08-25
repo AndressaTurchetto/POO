692bc8c3998198e2be6e4dc755563edb704f0ece
Andressa Turchetto <and.turchetto@gmail.com>
2024-08-25 20:02:34 -0300

    Adicionando alguns métodos

--- Admin.js
+++ Admin.js
@@ -4,8 +4,12 @@ class Admin extends User {
     constructor(nome, email, nascimento, role = "admin", ativo = "true") {
         super(nome, email, nascimento, role, ativo)
     }
+
+    criarCurso(nomeCurso, qtdVagas) {
+        return `curso ${nomeCurso} criado com ${qtdVagas} vagas.`
+    }
 }
 
 const novoAdmin = new Admin("Luiz", "luiz@gmail.com", "1987-08-04")
 console.log(novoAdmin);
-console.log(novoAdmin.exibirInfos());
+console.log(novoAdmin.criarCurso('JavaScript', '20'));
