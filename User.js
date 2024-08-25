2fb7ed444dc44d84d52a4457f6e553ca110a8549
Andressa Turchetto <and.turchetto@gmail.com>
2024-08-25 19:47:01 -0300

    Criando uma estrutura de classes

---
+++ User.js
@@ -0,0 +1,19 @@
+class User {
+    constructor(nome, email, nascimento, role, ativo = true) {
+      this.nome = nome
+      this.email = email
+      this.nascimento = nascimento
+      this.role = role || "estudante"
+      this.ativo = ativo
+    }
+  
+    exibirInfos() {
+      return `${this.nome}, ${this.email}`
+    }
+}
+
+const novoUser = new User('Andressa', 'and.turchetto@gmail.com', '2024-12-25')
+// console.log(novoUser);
+// console.log(novoUser.exibirInfos());
+
+console.log(User.prototype.isPrototypeOf(novoUser));
