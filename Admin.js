5d63363495ca0f8d29463ffd34750e5f771125e2
Andressa Turchetto <and.turchetto@gmail.com>
2024-08-25 20:12:39 -0300

    Estudando sobre polimorfismo

--- Admin.js
+++ Admin.js
@@ -1,6 +1,6 @@
 import User from "./User.js"
 
-class Admin extends User {
+export default class Admin extends User {
     constructor(nome, email, nascimento, role = "admin", ativo = "true") {
         super(nome, email, nascimento, role, ativo)
     }
@@ -10,6 +10,6 @@ class Admin extends User {
     }
 }
 
-const novoAdmin = new Admin("Luiz", "luiz@gmail.com", "1987-08-04")
-console.log(novoAdmin);
-console.log(novoAdmin.criarCurso('JavaScript', '20'));
+// const novoAdmin = new Admin("Luiz", "luiz@gmail.com", "1987-08-04")
+// console.log(novoAdmin);
+// console.log(novoAdmin.criarCurso('JavaScript', '20'));
